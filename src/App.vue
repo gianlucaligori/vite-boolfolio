36f166e20887a85aae596ccaa97523af6806ea30
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-17 17:21:40 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
 
 import ProjectList from './components/ProjectList.vue';
 import AppNav from './components/AppNav.vue';
+import AppFooter from './components/AppFooter.vue';
+
 
 
 export default {
   components: {
     ProjectList,
     AppNav,
+    AppFooter,
 
   },
 };
@@ -21,6 +24,7 @@ export default {
   <!-- <h1 class="text-center mt-2">BoolPrees</h1>
     <h2 class="text-center">Questi sono i miei progetti fatti con Boolean Anno 2023</h2> -->
   <ProjectList />
+  <AppFooter></AppFooter>
 </template>
 
 <style lang="scss">
